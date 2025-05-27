d3cd701025249d5a728a21f052fd59744918f980
Dims <mehdipares@85gmail.com>
2025-05-27 13:37:13 +0200

    footerpages

--- trouve-ton-artisan-front/src/pages/CookiesPage.jsx
+++ trouve-ton-artisan-front/src/pages/CookiesPage.jsx
@@ -1,8 +1,8 @@
 export default function CookiesPage() {
   return (
     <div className="container mt-5">
-      <h1>Cookies</h1>
-      <p>Page en construction.</p>
+      <h1>Politique de cookies</h1>
+      <p>Aucun cookie n’est utilisé sur ce site à des fins commerciales ou de suivi.</p>
     </div>
   );
 }
--- trouve-ton-artisan-front/src/pages/LegalPage.jsx
+++ trouve-ton-artisan-front/src/pages/LegalPage.jsx
@@ -1,8 +1,11 @@
 export default function LegalPage() {
   return (
     <div className="container mt-5">
-      <h1>Mentions Légales</h1>
-      <p>Page en construction.</p>
+      <h1>Mentions légales</h1>
+      <p>Ce site a été réalisé dans le cadre d’un projet pédagogique fictif.</p>
+      <p>Responsable : Mehdi Pares</p>
+      <p>Hébergement : Render.com & Alwaysdata</p>
+      <p>Adresse : 101 cours Charlemagne, 69002 Lyon</p>
     </div>
   );
 }
